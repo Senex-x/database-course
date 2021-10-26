40c36db6db64ce5088bd5b0b09f82472025d7ccf
V-Senex <vdm.snx@gmail.com>
2021-10-26 17:19:58 +0300

    Add graph schema ddl definition

---
+++ script-repository/graph-ddl.sql
@@ -0,0 +1,19 @@
+create table if not exists nodes
+(
+    id varchar(4) not null
+        constraint nodes_pk
+            primary key
+);
+
+alter table nodes owner to postgres;
+
+create table if not exists edges
+(
+    start_node_id varchar(4) not null,
+    end_node_id varchar(4) not null,
+    weight integer not null,
+    constraint edges_pk
+        primary key (start_node_id, end_node_id)
+);
+
+alter table edges owner to postgres;
