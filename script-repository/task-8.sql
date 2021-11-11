7961e2df473a1b0ca69b018d43b4f272ccc0f10d
V-Senex <junior.07.inator@gmail.com>
2021-11-11 23:58:31 +0300

    Update Task 8 script Add solutions for 1-5 subtasks

--- script-repository/task-8.sql
+++ script-repository/task-8.sql
@@ -1,77 +1,157 @@
-create table airports
-(
-    id serial
-        constraint airports_pk
-            primary key,
-    name varchar(64) not null,
-    location_city varchar(64) not null
-);
+-- Task 3
 
-alter table airports owner to postgres;
+-- Get the longest duration flight row
+SELECT *
+FROM flights
+         INNER JOIN (
+    SELECT MAX(duration) max_duration_column
+    FROM flights
+) max_duration_table ON duration = max_duration_table.max_duration_column;
 
-create unique index airports_name_uindex
-    on airports (name);
+-- Task 4
 
-create table airplanes
-(
-    id serial
-        constraint airplanes_pk
-            primary key,
-    model varchar(64) not null
-);
+-- Queries are split into subexpressions for easier readability step-by-step
+-- Get all flights for each airport
+SELECT airports.id airport_id, flights.id flight_id, flights.date flight_date
+FROM airports
+         JOIN flights
+              ON airports.id = flights.departure_airport_id
+                  OR airports.id = flights.arrival_airport_id
+ORDER BY airports.id;
 
-alter table airplanes owner to postgres;
+-- Get flights for each airport on the specified day
+SELECT airports.id airport_id, flights.id flight_id, flights.date flight_date
+FROM airports
+         JOIN flights
+              ON airports.id = flights.departure_airport_id
+                  OR airports.id = flights.arrival_airport_id
+WHERE date = '2021-11-11';
 
-create table passengers
-(
-    id serial
-        constraint passengers_pk
-            primary key,
-    name varchar(128) not null
-);
+-- Get the number of flights for each airport on the specified day
+SELECT airport_id, count(*) flights_count
+FROM (
+         SELECT airports.id airport_id, flights.id flight_id, flights.date flight_date
+         FROM airports
+                  JOIN flights
+                       ON airports.id = flights.departure_airport_id
+                           OR airports.id = flights.arrival_airport_id
+         WHERE date = '2021-11-11'
+     ) all_flights
+GROUP BY airport_id
+ORDER BY airport_id;
 
-alter table passengers owner to postgres;
+-- Task 4, but there is a nuance
 
-create table crew_members
-(
-    id serial
-        constraint crew_members_pk
-            primary key,
-    name varchar(128) not null,
-    position varchar(128) not null
-);
+-- Same job done by using 'with' to separate queries
+-- in a logical order and without duplications
+WITH all_flights AS (
+    SELECT airports.id airport_id, flights.id flight_id, flights.date flight_date
+    FROM airports
+             JOIN flights
+                  ON airports.id = flights.departure_airport_id
+                      OR airports.id = flights.arrival_airport_id
+),
+     all_flights_for_specified_day AS (
+         SELECT *
+         FROM all_flights
+         WHERE flight_date = '2021-11-11'
+     )
 
-alter table crew_members owner to postgres;
+SELECT airport_id, count(*) flights_count
+FROM all_flights_for_specified_day
+GROUP BY airport_id
+ORDER BY airport_id;
 
-create table flights
-(
-    id serial
-        constraint flights_pk
-            primary key,
-    departure_airport_id integer not null
-        constraint flights_airports_id_fk
-            references airports,
-    arrival_airport_id integer not null
-        constraint flights_airports_id_fk_2
-            references airports,
-    airplane_id integer not null
-        constraint flights_airplanes_id_fk
-            references airplanes,
-    duration integer not null,
-    date date not null
-);
+-- Task 5
+-- Step-by-step explanation
 
-alter table flights owner to postgres;
+-- Get all flight ids for all passengers
+SELECT passenger_id, flight_id
+FROM flight_passengers
+         JOIN passengers
+              ON flight_passengers.passenger_id = passengers.id
+ORDER BY flight_id;
 
-create table flight_crew_members
-(
-    flight_id integer not null
-        constraint flight_crew_members_flights_id_fk
-            references flights,
-    crew_member_id integer not null
-        constraint flight_crew_members_crew_members_id_fk
-            references crew_members
-);
+-- Get flight durations for every flight
+-- by join with 'flights' table
+SELECT passenger_id, flights.duration
+FROM (
+         SELECT passenger_id, flight_id
+         FROM flight_passengers
+                  JOIN passengers
+                       ON flight_passengers.passenger_id = passengers.id
+     ) all_flights
+         JOIN flights
+              ON all_flights.flight_id = flights.id
+ORDER BY flight_id;
 
-alter table flight_crew_members owner to postgres;
+-- Get passenger ids and their summarized total flight durations
+SELECT passenger_id, sum(duration) total_duration
+FROM (
+         SELECT passenger_id, flights.duration
+         FROM (
+                  SELECT passenger_id, flight_id
+                  FROM flight_passengers
+                           JOIN passengers
+                                ON flight_passengers.passenger_id = passengers.id
+              ) all_flights
+                  JOIN flights
+                       ON all_flights.flight_id = flights.id
+     ) all_flight_durations
+GROUP BY passenger_id
+ORDER BY passenger_id;
 
+-- Get two passengers with the most and the least total durations
+SELECT name, total_duration
+FROM passengers
+         INNER JOIN (
+    WITH total_durations AS (
+        SELECT passenger_id, sum(duration) total_duration
+        FROM (
+                 SELECT passenger_id, flights.duration
+                 FROM (
+                          SELECT passenger_id, flight_id
+                          FROM flight_passengers
+                                   JOIN passengers
+                                        ON flight_passengers.passenger_id = passengers.id
+                      ) all_flights
+                          JOIN flights
+                               ON all_flights.flight_id = flights.id
+             ) all_flight_durations
+        GROUP BY passenger_id
+    )
+    SELECT passenger_id, total_duration
+    FROM total_durations
+             JOIN (
+        SELECT MAX(total_duration) max_duration, MIN(total_duration) min_duration
+        FROM total_durations
+    ) max_duration_table
+                  ON total_duration = max_duration
+                      OR total_duration = min_duration
+) extremum_total_durations ON id = extremum_total_durations.passenger_id
+ORDER BY total_duration DESC;
+
+-- By the way
+-- Where is the mistake??
+SELECT *
+FROM (
+         SELECT passenger_id, sum(duration) total_duration
+         FROM (
+                  SELECT passenger_id, flights.duration
+                  FROM (
+                           SELECT passenger_id, flight_id
+                           FROM flight_passengers
+                                    JOIN passengers
+                                         ON flight_passengers.passenger_id = passengers.id
+                       ) all_flights
+                           JOIN flights
+                                ON all_flights.flight_id = flights.id
+              ) all_flight_durations
+         GROUP BY passenger_id
+     ) total_durations
+         INNER JOIN (
+    SELECT MAX(total_duration) max_col
+    FROM total_durations
+) max_dur_tbl ON total_durations.total_duration = max_dur_tbl.max_col;
+
+-- Task 6
